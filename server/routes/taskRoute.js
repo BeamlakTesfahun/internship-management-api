fe39238a2dc34a8259f3698f07d8eea8563aece3
Beamlak Tesfahun <beamlakt24@gmail.com>
2024-12-30 22:13:06 +0300

    added task management endpoints

---
+++ server/routes/taskRoute.js
@@ -0,0 +1,57 @@
+import express from "express";
+import {
+  validateCreateTask,
+  validateGetTasks,
+  validateUpdateTask,
+  validateGetStudentTasks,
+  validateDeleteTask,
+} from "../middlewares/taskValidator.js";
+import authenticateToken from "../middlewares/authenticateToken.js";
+import { authorizeRole } from "../middlewares/authorizeRole.js";
+import {
+  getTasks,
+  createTask,
+  updateTask,
+  deleteTask,
+  getStudentTasks,
+} from "../controllers/taskController.js";
+
+const router = express.Router();
+
+router.post(
+  "/create",
+  validateCreateTask,
+  authenticateToken,
+  authorizeRole(["admin"]),
+  createTask
+);
+router.get(
+  "/:trackId",
+  validateGetTasks,
+  authenticateToken,
+  authorizeRole(["admin"]),
+  getTasks
+);
+router.put(
+  "/update/:taskId",
+  validateUpdateTask,
+  authenticateToken,
+  authorizeRole(["admin"]),
+  updateTask
+);
+router.delete(
+  "/delete/:taskId",
+  validateDeleteTask,
+  authenticateToken,
+  authorizeRole(["admin"]),
+  deleteTask
+);
+router.get(
+  "/student/:userId",
+  validateGetStudentTasks,
+  authenticateToken,
+  authorizeRole(["student"]),
+  getStudentTasks
+);
+
+export default router;
