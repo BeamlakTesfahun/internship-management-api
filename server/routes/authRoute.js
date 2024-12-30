2e28b7c01f0a96f1baed122a6577aa2bad2fd7a2
Beamlak Tesfahun <beamlakt24@gmail.com>
2024-12-30 22:12:34 +0300

    added user management endpoints

---
+++ server/routes/authRoute.js
@@ -0,0 +1,62 @@
+import express from "express";
+import authenticateToken from "../middlewares/authenticateToken.js";
+import { authorizeRole } from "../middlewares/authorizeRole.js";
+import {
+  validateAdminRegistration,
+  validateInviteUser,
+  validateAccountSetup,
+  validateLogin,
+  validateForgotPassword,
+  validateResetPassword,
+  handleValidationErrors,
+} from "../middlewares/authValidator.js";
+import {
+  login,
+  inviteUser,
+  setupAccount,
+  forgotPassword,
+  resetPassword,
+  createAdmin,
+} from "../controllers/authController.js";
+
+const router = express.Router();
+
+router.post(
+  "/register-admin",
+  validateAdminRegistration,
+  handleValidationErrors,
+  createAdmin
+);
+
+router.post(
+  "/invite-user",
+  validateInviteUser,
+  handleValidationErrors,
+  authenticateToken,
+  authorizeRole(["admin"]),
+  inviteUser
+);
+
+router.post("/login/", validateLogin, handleValidationErrors, login);
+
+router.post(
+  "/setup-account/:inviteToken",
+  validateAccountSetup,
+  handleValidationErrors,
+  setupAccount
+);
+
+router.post(
+  "/forgot-password",
+  validateForgotPassword,
+  handleValidationErrors,
+  forgotPassword
+);
+router.post(
+  "/reset-password/:resetToken",
+  validateResetPassword,
+  handleValidationErrors,
+  resetPassword
+);
+
+export default router;
